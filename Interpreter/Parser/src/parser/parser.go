ce07e9a1580d0764735d137581b513932580a39c
husharp <jinhao.hu@pingcap.com>
2022-12-28 21:53:21 +0800

    [interpreter] finish parser let statement
    Signed-off-by: husharp <jinhao.hu@pingcap.com>

--- Interpreter/Parser/src/parser/parser.go
+++ Interpreter/Parser/src/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+	"src/ast"
 	"src/lexer"
 	"src/token"
 )
@@ -8,12 +10,13 @@ import (
 type Parser struct {
 	l *lexer.Lexer
 
+	errors    []string
 	curToken  token.Token
 	peekToken token.Token // support for more info
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 
 	// make both token be set
 	p.nextToken()
@@ -25,4 +28,74 @@ func New(l *lexer.Lexer) *Parser {
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) expectPeek(expect token.TokenType) bool {
+	if expect == p.peekToken.Type {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(expect)
+		return false
+	}
+}
+
+func (p *Parser) curTokenIs(expect token.TokenType) bool {
+	return expect == p.curToken.Type
+}
+
+func (p *Parser) Error() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) parseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{
+		Token: p.curToken,
+	}
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+	stmt.Name = &ast.Identifier{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// skip expression now
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
 }
