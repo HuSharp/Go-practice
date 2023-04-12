a6c83c0912ce787908cadcd65ca89bb5e61d7e13
husharp <jinhao.hu@pingcap.com>
2023-04-12 23:01:53 +0800

    [compiler] move builtin to object
    Signed-off-by: husharp <jinhao.hu@pingcap.com>

--- Compiler/Bytecode/src/evaluator/evaluator_test.go
+++ Compiler/Bytecode/src/evaluator/evaluator_test.go
@@ -328,10 +328,10 @@ func TestBuiltinFunctions(t *testing.T) {
 		evaluated := testEval(tt.input)
 
 		switch expected := tt.expected.(type) {
-		//case int:
-		//	testIntegerObject(t, evaluated, int64(expected))
-		//case nil:
-		//	testNullObject(t, evaluated)
+		case int:
+			testIntegerObject(t, evaluated, int64(expected))
+		case nil:
+			testNullObject(t, evaluated)
 		case string:
 			errObj, ok := evaluated.(*object.Error)
 			if !ok {
@@ -343,22 +343,22 @@ func TestBuiltinFunctions(t *testing.T) {
 				t.Errorf("wrong error message. expected=%q, got=%q",
 					expected, errObj.Message)
 			}
-			//case []int:
-			//	array, ok := evaluated.(*object.Array)
-			//	if !ok {
-			//		t.Errorf("obj not Array. got=%T (%+v)", evaluated, evaluated)
-			//		continue
-			//	}
-			//
-			//	if len(array.Elements) != len(expected) {
-			//		t.Errorf("wrong num of elements. want=%d, got=%d",
-			//			len(expected), len(array.Elements))
-			//		continue
-			//	}
-			//
-			//	for i, expectedElem := range expected {
-			//		testIntegerObject(t, array.Elements[i], int64(expectedElem))
-			//	}
+		case []int:
+			array, ok := evaluated.(*object.Array)
+			if !ok {
+				t.Errorf("obj not Array. got=%T (%+v)", evaluated, evaluated)
+				continue
+			}
+
+			if len(array.Elements) != len(expected) {
+				t.Errorf("wrong num of elements. want=%d, got=%d",
+					len(expected), len(array.Elements))
+				continue
+			}
+
+			for i, expectedElem := range expected {
+				testIntegerObject(t, array.Elements[i], int64(expectedElem))
+			}
 		}
 	}
 }
